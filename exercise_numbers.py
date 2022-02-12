78ebc6e21b95b586d85096e1968deed1fe94b35f
Maxcode123 <nikiforos@live.co.uk>
2022-02-12 13:04:54 +0200

    exercise numbers generator

---
+++ exercise_numbers.py
@@ -0,0 +1,11 @@
+import random, hashlib
+
+am = "00000"
+
+tmp = hashlib.sha256(am.encode()).hexdigest()
+
+seed = int(tmp, 16)
+random.seed(seed)
+lst = list(range(1, 14))
+random.shuffle(lst)
+print(lst[:4])
